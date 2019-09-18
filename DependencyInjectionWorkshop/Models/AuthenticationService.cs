0b396305888302bc32764b1274e9363dc731a4b6
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 14:48:51 +0800

    rename IProfile relative fileds and variables

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -8,13 +8,13 @@ namespace DependencyInjectionWorkshop.Models
         private readonly FailedCounter _failedCounter;
         private readonly NLogAdapter _nLogAdapter;
         private readonly OtpService _otpService;
-        private readonly IProfile _profileDao;
+        private readonly IProfile _profile;
         private readonly Sha256Adapter _sha256Adapter;
         private readonly SlackAdapter _slackAdapter;
 
         public AuthenticationService()
         {
-            _profileDao = new ProfileDao();
+            _profile = new ProfileDao();
             _sha256Adapter = new Sha256Adapter();
             _otpService = new OtpService();
             _failedCounter = new FailedCounter();
@@ -29,13 +29,13 @@ namespace DependencyInjectionWorkshop.Models
                 throw new FailedTooManyTimesException();
             }
 
-            var passwordFromDb = _profileDao.GetPassword(accountId);
+            var currentPassword = _profile.GetPassword(accountId);
 
             var hashedPassword = _sha256Adapter.ComputeHashedPassword(password);
 
             var currentOtp = _otpService.GetCurrentOtp(accountId);
 
-            if (passwordFromDb == hashedPassword && otp == currentOtp)
+            if (currentPassword == hashedPassword && otp == currentOtp)
             {
                 _failedCounter.ResetFailedCount(accountId);
 
