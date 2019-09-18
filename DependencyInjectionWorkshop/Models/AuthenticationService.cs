4de5e015b4e871d6995d91a184f2ca98f83171fd
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 13:50:46 +0800

    add GetHash()

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using Dapper;
 
 namespace DependencyInjectionWorkshop.Models
@@ -18,6 +19,18 @@ namespace DependencyInjectionWorkshop.Models
             }
 
             throw new NotImplementedException();
+        }
+
+        public string GetHash(string plainText)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
