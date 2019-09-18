194ea467dfec7452fce6a0530448224d91984443
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 15:14:59 +0800

    extract field: "_authenticationService" and initialize in SetUp()

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -7,6 +7,7 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private AuthenticationService _authenticationService;
         private IFailedCounter _failedCounter;
         private IHash _hash;
         private ILogger _logger;
@@ -23,19 +24,18 @@ namespace DependencyInjectionWorkshopTests
             _otpService = Substitute.For<IOtpService>();
             _hash = Substitute.For<IHash>();
             _profile = Substitute.For<IProfile>();
+            _authenticationService =
+                new AuthenticationService(_failedCounter, _hash, _logger, _notification, _otpService, _profile);
         }
 
         [Test]
         public void is_valid()
         {
-            var authenticationService =
-                new AuthenticationService(_failedCounter, _hash, _logger, _notification, _otpService, _profile);
-
             _profile.GetPassword("joey").Returns("my hashed password");
             _hash.Compute("abc").Returns("my hashed password");
             _otpService.GetCurrentOtp("joey").Returns("123456");
 
-            var isValid = authenticationService.Verify("joey", "abc", "123456");
+            var isValid = _authenticationService.Verify("joey", "abc", "123456");
             Assert.IsTrue(isValid);
         }
     }
