8a502ed384e6460438a2c591fa1efee74b603582
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 13:48:29 +0800

    add GetPassword()

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
 
 namespace DependencyInjectionWorkshop.Models
 {
@@ -7,6 +11,17 @@ namespace DependencyInjectionWorkshop.Models
         public bool Verify(string accountId, string password, string otp)
         {
             throw new NotImplementedException();
+        }
+
+        public string GetPassword(string accountId)
+        {
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var password = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                                                        commandType: CommandType.StoredProcedure).SingleOrDefault();
+
+                return password;
+            }
         }
     }
 }
