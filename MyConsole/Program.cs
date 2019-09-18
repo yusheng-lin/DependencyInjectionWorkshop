1b0aeeaef238c57f602a26be6c80eb81a72f8ae6
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 17:56:55 +0800

    add LogMethodInfoDecorator for logging parameter and result value of AuthenticationService

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -38,12 +38,36 @@ namespace MyConsole
             builder.RegisterType<NotificationDecorator>();
             builder.RegisterType<FailedCounterDecorator>();
             builder.RegisterType<LogDecorator>();
+            builder.RegisterType<LogMethodInfoDecorator>();
 
             builder.RegisterDecorator<NotificationDecorator, IAuthentication>();
             builder.RegisterDecorator<FailedCounterDecorator, IAuthentication>();
             builder.RegisterDecorator<LogDecorator, IAuthentication>();
+            builder.RegisterDecorator<LogMethodInfoDecorator, IAuthentication>();
 
             _container = builder.Build();
+        }
+    }
+
+    internal class LogMethodInfoDecorator : AuthenticationBaseDecorator
+    {
+        private readonly ILogger _logger;
+
+        public LogMethodInfoDecorator(IAuthentication authentication, ILogger logger) : base(authentication)
+        {
+            _logger = logger;
+        }
+
+        public override bool Verify(string accountId, string password, string otp)
+        {
+            var message = $"{nameof(AuthenticationService)}.{nameof(Verify)}:{accountId} | {password} |{otp}";
+            _logger.Info(message);
+
+            var isValid = base.Verify(accountId, password, otp);
+
+            _logger.Info($"{accountId} isValid: {isValid.ToString()}");
+
+            return isValid;
         }
     }
 
