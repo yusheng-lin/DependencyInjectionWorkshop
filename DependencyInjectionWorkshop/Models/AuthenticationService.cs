9b358e0d518a7c1b0fbac2d8e69d6812a751046e
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 13:48:15 +0800

    add Verify()

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,12 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
