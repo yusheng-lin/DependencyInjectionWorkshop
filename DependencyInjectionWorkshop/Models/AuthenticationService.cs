831c3f920d36e65ef129ccfd2c3d5addbe51d2e3
Joey Chen <jay.hatelove@gmail.com>
2019-09-18 13:51:30 +0800

    inline GetHash()

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -18,19 +18,17 @@ namespace DependencyInjectionWorkshop.Models
                                                           commandType: CommandType.StoredProcedure).SingleOrDefault();
             }
 
-            throw new NotImplementedException();
-        }
-
-        public string GetHash(string plainText)
-        {
             var crypt = new System.Security.Cryptography.SHA256Managed();
             var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
             foreach (var theByte in crypto)
             {
                 hash.Append(theByte.ToString("x2"));
             }
-            return hash.ToString();
+
+            var hashedPassword = hash.ToString();
+
+            throw new NotImplementedException();
         }
     }
 }
