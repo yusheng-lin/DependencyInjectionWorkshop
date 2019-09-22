e66ddcd9cd3fc5cfc99bbb1fa32383e414a3ac3f
yushenglin <poallin@gmail.com>
2019-09-22 15:19:53 +0800

    using autofac

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,113 +1,124 @@
-﻿using DependencyInjectionWorkshop.Models;
+﻿using Autofac;
+using DependencyInjectionWorkshop.Models;
 using System;
 
 namespace MyConsole
 {
     class Program
     {
-        private static INotification _notification;
-        private static ILogger _logger;
-        private static IOtpService _otpService;
-        private static IFailedCounter _failedCounter;
-        private static IHash _hash;
-        private static IProfile _profile;
+        private static IContainer _container;
         private static IAuthentication _authentication;
 
         static void Main(string[] args)
         {
-            _notification = new FakeSlack();
-            _logger = new FakeLogger();
-            _failedCounter = new FakeFailedCounter();
-            _otpService = new FakeOtp();
-            _hash = new FakeHash();
-            _profile = new FakeProfile();
-            _authentication =
-                new AuthenticationService(_otpService, _profile, _hash);
+            RegisterContainer();
 
-            _authentication = new NotificationDecorator(_authentication, _notification);
-            _authentication = new FailedCounterDecorator(_authentication, _failedCounter);
-            _authentication = new LogFailedCountDecorator(_authentication, _failedCounter, _logger);
+            _authentication = _container.Resolve<IAuthentication>();
 
             var isValid = _authentication.Verify("joey", "abc", "wrong otp");
             Console.WriteLine($"result:{isValid}");
         }
-    }
 
-    internal class FakeLogger : ILogger
-    {
-        public void LogInfo(string message)
+        private static void RegisterContainer()
         {
-            Console.WriteLine(message);
-        }
-    }
+            var builder = new ContainerBuilder();
 
-    internal class FakeSlack : INotification
-    {
-        public void PushMessage(string message)
-        {
-            Console.WriteLine(message);
-        }
+            builder.RegisterType<FakeProfile>().As<IProfile>();
+            builder.RegisterType<FakeHash>().As<IHash>();
+            builder.RegisterType<FakeOtp>().As<IOtpService>();
+            builder.RegisterType<FakeLogger>().As<ILogger>();
+            builder.RegisterType<FakeSlack>().As<INotification>();
+            builder.RegisterType<FakeFailedCounter>().As<IFailedCounter>();
+            builder.RegisterType<AuthenticationService>().As<IAuthentication>();
 
-        public void Send(string accountId)
-        {
-            PushMessage($"{nameof(Send)}, accountId:{accountId}");
-        }
-    }
+            builder.RegisterDecorator<NotificationDecorator, IAuthentication>();
+            builder.RegisterDecorator<FailedCounterDecorator, IAuthentication>();
+            builder.RegisterDecorator<LogFailedCountDecorator, IAuthentication>();
 
-    internal class FakeFailedCounter : IFailedCounter
-    {
-        public void ResetFailedCount(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(ResetFailedCount)}({accountId})");
-        }
-
-        public void AddFailedCount(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(AddFailedCount)}({accountId})");
-        }
-
-        public bool GetAccountIsLocked(string accountId)
-        {
-            return IsAccountLocked(accountId);
-        }
-
-        public int GetFailedCount(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(GetFailedCount)}({accountId})");
-            return 91;
-        }
-
-        public bool IsAccountLocked(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(IsAccountLocked)}({accountId})");
-            return false;
-        }
-    }
-
-    internal class FakeOtp : IOtpService
-    {
-        public string GetCurrentOtp(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeOtp)}.{nameof(GetCurrentOtp)}({accountId})");
-            return "123456";
-        }
-    }
-
-    internal class FakeHash : IHash
-    {
-        public string Compute(string plainText)
-        {
-            Console.WriteLine($"{nameof(FakeHash)}.{nameof(Compute)}({plainText})");
-            return "my hashed password";
-        }
-    }
-
-    internal class FakeProfile : IProfile
-    {
-        public string GetPassword(string accountId)
-        {
-            Console.WriteLine($"{nameof(FakeProfile)}.{nameof(GetPassword)}({accountId})");
-            return "my hashed password";
+            //_authentication = new NotificationDecorator(_authentication, _notification);
+            //_authentication = new FailedCounterDecorator(_authentication, _failedCounter);
+            //_authentication = new LogFailedCountDecorator(_authentication, _logger, _failedCounter);
+            var container = builder.Build();
+            _container = container;
         }
     }
 }
+
+internal class FakeLogger : ILogger
+{
+    public void LogInfo(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+internal class FakeSlack : INotification
+{
+    public void PushMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Send(string accountId)
+    {
+        PushMessage($"{nameof(Send)}, accountId:{accountId}");
+    }
+}
+
+internal class FakeFailedCounter : IFailedCounter
+{
+    public void ResetFailedCount(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(ResetFailedCount)}({accountId})");
+    }
+
+    public void AddFailedCount(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(AddFailedCount)}({accountId})");
+    }
+
+    public bool GetAccountIsLocked(string accountId)
+    {
+        return IsAccountLocked(accountId);
+    }
+
+    public int GetFailedCount(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(GetFailedCount)}({accountId})");
+        return 91;
+    }
+
+    public bool IsAccountLocked(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(IsAccountLocked)}({accountId})");
+        return false;
+    }
+}
+
+internal class FakeOtp : IOtpService
+{
+    public string GetCurrentOtp(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeOtp)}.{nameof(GetCurrentOtp)}({accountId})");
+        return "123456";
+    }
+}
+
+internal class FakeHash : IHash
+{
+    public string Compute(string plainText)
+    {
+        Console.WriteLine($"{nameof(FakeHash)}.{nameof(Compute)}({plainText})");
+        return "my hashed password";
+    }
+}
+
+internal class FakeProfile : IProfile
+{
+    public string GetPassword(string accountId)
+    {
+        Console.WriteLine($"{nameof(FakeProfile)}.{nameof(GetPassword)}({accountId})");
+        return "my hashed password";
+    }
+}
+
